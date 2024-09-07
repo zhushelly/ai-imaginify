cdf91e2a377c463c11520c2cec84455b9fb1a0f2
Shelly Zhu <zhixiuzhu@umail.ucsb.edu>
2024-09-07 02:38:55 -0700

    transformed image component

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -27,6 +27,8 @@ import { CustomField } from "./CustomField"
 import { useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import MediaUploader from "./MediaUploader"
+import TransformedImage from "./TransformedImage"
+import { updateCredits } from "@/lib/actions/user.actions"
 
 export const formSchema = z.object({
   title: z.string(),
@@ -106,7 +108,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
             )
             setNewTransformation(null)
             startTransition(async () => {
-                // await updateCredits(userId, creditFee)
+                await updateCredits(userId, -1)
             })
 
         }
@@ -209,7 +211,14 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
                     />
                 )}
             />
-
+            <TransformedImage
+                image={image}
+                type={type}
+                title={form.getValues().title}
+                isTransforming={isTransforming}
+                setIsTransforming={setIsTransforming}
+                transformationConfig={transformationConfig}
+            />
         </div>
 
         <div className="flex flex-col gap-4">
