15c4bc8407da2ffa45397b1091314bb0fef381d8
Shelly Zhu <zhixiuzhu@umail.ucsb.edu>
2024-09-08 22:52:51 -0700

    image server action done

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -14,9 +14,9 @@ import {
   } from "@/components/ui/select"
   
 import { Input } from "@/components/ui/input"
-import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants"
+import { aspectRatioOptions, creditFee, defaultValues, transformationTypes } from "@/constants"
 import { CustomField } from "./CustomField"
-import { useState, useTransition } from "react"
+import { useEffect, useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import MediaUploader from "./MediaUploader"
 import TransformedImage from "./TransformedImage"
@@ -24,6 +24,7 @@ import { updateCredits } from "@/lib/actions/user.actions"
 import { getCldImageUrl } from "next-cloudinary"
 import { addImage, updateImage } from "@/lib/actions/image.actions"
 import { useRouter } from "next/navigation"
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
 
 export const formSchema = z.object({
   title: z.string(),
@@ -164,14 +165,21 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
             )
             setNewTransformation(null)
             startTransition(async () => {
-                await updateCredits(userId, -1)
+                await updateCredits(userId, creditFee)
             })
 
         }
 
+        useEffect(() => {
+            if (image && (type === 'restore' || type === 'removeBackground')) {
+                setNewTransformation(transformationType.config)
+            }
+        }, [image, transformationType.config, type])
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField
             control={form.control}
             name="title"
