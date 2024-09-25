0772abd8e95e226dbed89acf943f7725162c17d0
Shelly Zhu <zhixiuzhu@umail.ucsb.edu>
2024-09-25 16:14:28 -0700

    edited home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,19 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { navLinks } from '@/constants'
-import React from 'react'
-import { Collection } from '@/components/shared/Collection'
-import { getAllImages } from '@/lib/actions/image.actions'
+import { Collection } from "@/components/shared/Collection"
+import { navLinks } from "@/constants"
+import { getAllImages } from "@/lib/actions/image.actions"
+import Image from "next/image"
+import Link from "next/link"
 
 const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
-  
-  const images = await getAllImages({ page, searchQuery })
 
+  const images = await getAllImages({ page, searchQuery})
 
   return (
     <>
-      <section className="home-heading">
-        <h1>
+      <section className="home">
+        <h1 className="home-heading">
           Unleash Your Creative Vision with Imaginify
         </h1>
         <ul className="flex-center w-full gap-20">
@@ -29,10 +27,8 @@ const Home = async ({ searchParams }: SearchParamProps) => {
                 <Image src={link.icon} alt="image" width={24} height={24} />
               </li>
               <p className="p-14-medium text-center text-white">{link.label}</p>
-
             </Link>
           ))}
-
         </ul>
       </section>
 
@@ -43,7 +39,6 @@ const Home = async ({ searchParams }: SearchParamProps) => {
           totalPages={images?.totalPage}
           page={page}
         />
-
       </section>
     </>
   )
